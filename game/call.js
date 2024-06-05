5aa4c576799868ceff801c46cf6e078bb01166e6
Tushar Kumar Singh <tusharsingh102003@gmail.com>
2024-06-05 22:13:35 +0530

    2

---
+++ game/call.js
@@ -0,0 +1,17 @@
+let val2 = 20
+let val1 = 10
+function addnum(val2 , val1)
+{
+   let total = num1 + num2
+   return total
+}
+ let result1 = addnum(val1, val2)
+ let result2 = addnum(3,4)
+
+
+ // steps
+ //1. global executon add .this
+ //2. memory phase where all var are stored var-> undefined 
+ //3. memory pahse m hei addnum jaega line 3 jismai addnum-> definination hoga uska 
+ // result 1 -> undefined result2-> undefined 
+ // execution phase
